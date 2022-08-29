5731023e79038cbf615537c8f978be4499a969ea
theHowToProject <thehowtoproject123@gmail.com>
2022-08-29 14:39:20 +0200

    back to localhost again

--- src/web/web.ts
+++ src/web/web.ts
@@ -25,7 +25,7 @@ export function getEndpoints(
 
 	// cors options
 	const corsOptions = {
-		origin: '*',
+		origin: 'http://localhost:3000',
 		optionsSuccessStatus: 200,
 	};
 	app.use(cors(corsOptions));
